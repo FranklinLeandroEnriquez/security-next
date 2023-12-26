76b9b5fc628c5ae5d63d58b02116181ad7a44319
FranklinLeandroEnriquez <flenriquez@uce.edu.ec>
2023-12-25 19:00:22 -0500

    Proyecto Seguridad

---
+++ src/components/MaxWidthWrapper.tsx
@@ -0,0 +1,24 @@
+import { cn } from "@/lib/utils"
+import { ReactNode } from "react"
+
+
+const MaxWidthWrapper = ({
+    clasName,
+    children,
+}:
+    {
+        clasName?: string,
+        children: ReactNode,
+    }) => {
+    return (
+        <div className={cn(
+            'mx-auto w-full max-w-screen-xl px-2.5 md:px-20',
+            clasName
+        )}>
+            {children}
+        </div>
+    )
+
+}
+
+export default MaxWidthWrapper
