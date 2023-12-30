1ad3f86e826be251808cf9a5850ae9ad4315ccf6
FranklinLeandroEnriquez <flenriquez@uce.edu.ec>
2023-12-30 04:46:22 -0500

    V1-style Table Franklin

--- src/app/dashboard/user/page.tsx
+++ src/app/dashboard/user/page.tsx
@@ -1,12 +1,15 @@
 'use client';
 
-import { deleteUser, getUsers } from "@/services/User/UserService";
+import { DataTable } from '@/components/data-table'
+import { columns, User } from '@/app/dashboard/user/columns'
+import MaxWidthWrapper from "@/components/MaxWidthWrapper"
+
+import { deleteUser } from "@/services/User/UserService";
 import { UserResponse } from "@/types/User/UserResponse";
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from "react";
 
-
-export default function User() {
+export default function Page() {
     const [users, setUsers] = useState<UserResponse[]>([] as UserResponse[]);
 
     const router = useRouter();
@@ -14,7 +17,7 @@ export default function User() {
     const deleteUserHandler = async (id: number) => {
         await deleteUser(id).then((res) => {
             if (res.status === 200) {
-                return getUsersHandler();
+                return getUsersLocal();
             }
             window.alert('Error');
         }).catch((err) => {
@@ -30,62 +33,20 @@ export default function User() {
         router.push(`/dashboard/user/create`);
     }
 
-    const getUsersHandler = async () => {
-        await getUsers().then((res) => {
-            if (res.status === 200) {
-                return res.json().then((data) => {
-                    setUsers(data);
-                });
-            }
-            window.alert('Error');
-        }).catch((err) => {
-            window.alert('Error');
-        });
+    const getUsersLocal = async () => {
+        const res = await fetch('http://localhost:3000/api/users');
+        const data: UserResponse[] = await res.json();
+        setUsers(data);
     }
 
     useEffect(() => {
-        getUsersHandler();
-    }, [users.length]);
+        getUsersLocal();
+    }, []);
 
     return (
-        <div>
-            <h1>User</h1>
-            <button onClick={createUserHandler}>CREATE </button>
-
-            <table>
-                <thead>
-                    <tr>
-                        <th>Actions</th>
-                        <th>ID</th>
-                        <th>NAME</th>
-                        <th>EMAIL</th>
-                        <th>DNI</th>
-                        <th>STATUS</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {users.map((user: UserResponse) => (
-                        <tr key={user.id}>
-                            <td>
-                                <ul>
-                                    <li>
-                                        <button onClick={(e) => updateUserHandler(user.id)}>UPDATE </button>
-                                    </li>
-                                    <li>
-                                        <button onClick={(e) => deleteUserHandler(user.id)}>DELETE </button>
-                                    </li>
-                                </ul>
-                            </td>
-                            <td>{user.id}</td>
-                            <td>{user.username}</td>
-                            <td>{user.email}</td>
-                            <td>{user.dni}</td>
-                            <td>{user.status ? 'True' : 'Flase'}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    );
-};
-
+        <MaxWidthWrapper>
+            <h1> ALL USERS</h1>
+            <DataTable<User, string> columns={columns(updateUserHandler, deleteUserHandler)} data={users} />
+        </MaxWidthWrapper>
+    )
+}
