19dff7bb35aa4990bb5162d62e22cbeb2af3a95d
BLeo16 <bleonardo1621@gmail.com>
2023-12-31 00:27:55 -0500

    Services and types Functions

---
+++ src/types/Function/CreateFunctionRequest.ts
@@ -0,0 +1,4 @@
+export interface CreateFunctionRequest {
+    name: string;
+    moduleId: number;
+}
---
+++ src/types/Function/UpdateFunctionRequest.ts
@@ -0,0 +1,5 @@
+export interface UpdateFunctionRequest {
+    name: string;
+    moduleId: number;
+    status: boolean;
+}
