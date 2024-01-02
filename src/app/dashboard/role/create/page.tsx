5856388d6a16b71f2f17cc76e5f5d33c01202307
BLeo16 <bleonardo1621@gmail.com>
2024-01-02 13:30:06 -0500

    role forms

--- src/app/dashboard/role/create/page.tsx
+++ src/app/dashboard/role/create/page.tsx
@@ -6,6 +6,33 @@ import { ErrorResponse, ValidationErrorResponse } from "@/types/shared/Validatio
 import { useRouter } from "next/navigation"
 import { useEffect, useState } from "react"
 
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import Header from "@/components/Header"
+
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+
+import { Input } from "@/components/ui/input"
+import { toast } from "sonner"
+import path from "path"
+
 export default function RoleCreateForm() {
     const [role, setRole] = useState<CreateRoleRequest>({} as CreateRoleRequest)
     const [errors, setErrors] = useState<ValidationErrorResponse | null>(null)
@@ -13,61 +40,96 @@ export default function RoleCreateForm() {
 
     const router = useRouter()
 
-    const onSubmit = async (e: any) => {
-        e.preventDefault()
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
         try {
-            await createRole(role).then(async (res) => {
-                if (res.status === 201) {
-                    return router.push("/dashboard/role")
-                }
+            const res = await createRole(values)
+            if (res.status === 201) {
+                toast.success("Role created successfully")
+                return router.push("/dashboard/role")
+            }
 
-                await res.json().then((data: ValidationErrorResponse) => {
-                    if (data.error == 'ValidationException') {
-                        setErrorResponse(null)
-                        return setErrors(data)
-                    }
-
-                    setErrors(null)
-                    return setErrorResponse({
-                        error: data.error,
-                        message: data.message.toString(),
-                        statusCode: data.statusCode,
-                        path: data.path,
-                        date: data.date,
-                    })
-
+            const data: ValidationErrorResponse = await res.json()
+            if (data.error == 'ValidationException') {
+                setErrorResponse(null)
+                setErrors(data)
+                toast.error(data.message.toString())
+            } else {
+                setErrors(null)
+                setErrorResponse({
+                    error: data.error,
+                    message: data.message.toString(),
+                    statusCode: data.statusCode,
+                    path: data.path,
+                    date: data.date
                 })
-
-            }).catch((err) => {
-                return window.alert('Error')
-            })
-        } catch (error) {
-            window.alert(error)
+                toast.error(data.message.toString())
+            }
+        } catch (err) {
+            if(err instanceof Error){
+                toast.error(err.message.toString())
+            } else{
+                toast.error("An error has occurred")
+            }
         }
     }
 
+    const formSchema = z.object({
+        name: z.string().min(5, {
+            message: "Name must be at least 5 characters long",
+        }).max(50,{
+            message: "Name must be less than 50 characters long",
+        }),
+
+    })
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+        }
+    })
     return (
-        <div>
-            <h1>Crear</h1>
+        <>
+            <Header title="Create Role" />
+            <div className="flex justify-center items-center mt-10">
+                <Card className="w-[40%]">
+                    <CardHeader>
+                        <CardTitle>Create Role</CardTitle>
+                        <CardDescription>Role Creation - Security Module</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <Form {...form}>
+                            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+                                <FormField
+                                    control={form.control}
+                                    name="name"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel htmlFor="name">Name</FormLabel>
+                                            <FormControl>
+                                                <Input
+                                                    placeholder="Name..."
+                                                    {...field}
+                                                />
+                                            </FormControl>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                                <div className="flex justify-end">
+                                    <Button variant="outline" type="button" onClick={()=> router.push("/dashboard/role")}>Cancel</Button>
+                                    <Button 
+                                        type="submit"
+                                        value="Save"
+                                    >Create</Button>
+                                </div>
+                            </form>
+                        </Form>
+                    </CardContent>
+                </Card>
+            </div>
+        </>
 
-            {errorResponse?.message}
-
-            <form onSubmit={onSubmit}>
-                {errors?.message?.find((err) => err.field === 'name')?.errors}
-                <input
-                    type="text"
-                    placeholder="Write a name"
-                    onChange={(e) => setRole({ ...role, name: e.target.value })}
-                    value = {role.name}
-                    required
-                />
-            
-                <input
-                    type="submit"
-                    value="Save"
-                />
-            </form>
-        </div>
     )
 
 }
