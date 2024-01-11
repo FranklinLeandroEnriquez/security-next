b05d8d471cee1aba7739fa70a3d8583d8246fcb6
BLeo16 <bleonardo1621@gmail.com>
2024-01-11 16:48:50 -0500

    Add audit for role

--- src/app/dashboard/role/page.tsx
+++ src/app/dashboard/role/page.tsx
@@ -12,19 +12,38 @@ import { Button } from '@/components/ui/button';
 import { UserCheck } from 'lucide-react';
 import Header from '@/components/Header';
 import { toast } from "sonner";
+import { getIp,logAuditAction } from '@/services/Audit/AuditService';
+import { useAuthToken } from '@/hooks/useAuthToken';
 
 export default function Page() {
     const [roles, setRoles] = useState<RoleResponse[]>([] as RoleResponse[]);
     const [errors, setErrors] = useState<ErrorResponse | null>(null);
     const [errorResponse, setErrorResponse] = useState<ErrorResponse | null>(null);
     const router = useRouter();
+    const token = useAuthToken();
 
     const deleteRoleHandler = async (id: number) => {
+        const ip = await getIp();
         await deleteRole(id).then(async (res) => {
             if (res.status === 200) {
+
+                await logAuditAction({
+                    functionName: 'SEC-ROLES-DELETE',
+                    action: 'delete Role',
+                    description: 'Successfully deleted role',
+                    observation: `Role id: ${id}`,
+                    ip: ip.toString(),
+                }, token);
                 getRolesHandler();
                 toast.success("Role deleted successfully");
+
             } else {
+                await logAuditAction({
+                    functionName: 'SEC-ROLES-DELETE',
+                    action: 'delete Role',
+                    description: 'Failed to delete role',
+                    ip: ip.toString(),
+                }, token);
                 const errorData: ErrorResponse = await res.json();
                 if (errorData.error === 'ErrorResponse') {
                     setErrorResponse(null);
@@ -47,13 +66,29 @@ export default function Page() {
     }
 
     const getRolesHandler = async () => {
-        await getRoles().then((res) => {
+        const ip = await getIp();
+        await getRoles().then(async (res) => {
             if (res.status === 200) {
+                await logAuditAction({
+                    functionName: 'SEC-ROLES-READ',
+                    action: 'get Roles',
+                    description: 'Successfully fetched roles',
+                    ip: ip.toString(),
+
+                }, token);
                 return res.json().then((data) => {
                     setRoles(data);
                 });
+            } else {
+                await logAuditAction({
+                    functionName: 'SEC-ROLES-READ',
+                    action: 'get Roles',
+                    description: 'Failed to fetch roles',
+                    ip: ip.toString(),
+                }, token);
+
+                toast.error('An error has occurred');
             }
-            toast.error('An error has occurred');
         }).catch((err) => {
             toast.error('An error has occurred');
         });
