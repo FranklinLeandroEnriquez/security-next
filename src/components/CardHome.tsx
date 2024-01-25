da126dd83d9f8a940a9bc4a637f68f894d4419b3
SANTIAGO FIERRO <santimarcelofierrod@gmail.com>
2024-01-25 00:39:47 -0500

    Update dependencies and refactor CardHome component (#51)

--- src/components/CardHome.tsx
+++ src/components/CardHome.tsx
@@ -3,28 +3,35 @@
 import React from "react";
 import { LucideIcon } from "lucide-react";
 import { cn } from "@/lib/utils";
+import CountUp from 'react-countup';
 
 export type CardProps = {
     label: string;
     icon: LucideIcon;
-    amount: string;
+    amount: number;
     description: string;
 };
 
 export default function CardHome(props: CardProps) {
+    const duration = Math.max(1, 5 / Math.log(props.amount + 1));
     return (
-        <CardContent>
-            <section className="flex justify-between gap-2">
-                {/* label */}
-                <p className="text-sm">{props.label}</p>
-                {/* icon */}
-                <props.icon className="h-4 w-4 text-gray-400" />
-            </section>
-            <section className="flex flex-col gap-1">
-                <h2 className="text-2xl font-semibold">{props.amount}</h2>
-                <p className="text-xs text-gray-500">{props.description}</p>
-            </section>
-        </CardContent>
+        <div className="relative aspect-square min-w-[200px] min-h-[250px] md:min-w-[200px] md:min-h-[250px]">
+            <CardContent className="flex flex-col items-center justify-between h-full p-4">
+                <p className="text-lg md:text-xl lg:text-4xl truncate w-full text-center font-semibold text-gray-500">
+                    {props.label}
+                </p>
+
+                <props.icon className="h-8 w-8 md:h-12 md:w-12 text-gray-400 my-2" />
+
+                <h2 className="text-2xl md:text-4xl lg:text-6xl font-bold">
+                    <CountUp end={props.amount} duration={duration} />
+                </h2>
+
+                <p className="text-sm md:text-lg lg:text-2xl truncate w-full text-center text-gray-500">
+                    {props.description}
+                </p>
+            </CardContent>
+        </div>
     );
 }
 
