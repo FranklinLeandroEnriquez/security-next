4ad6344c6cea3f0c6ae7943e08ee329a52280d3a
BLeo16 <bleonardo1621@gmail.com>
2024-01-28 22:14:50 -0500

    Add basic reports for functions, modules, roles, and users

--- src/types/Reports/users/userReports.tsx
+++ src/types/Reports/users/userReports.tsx
@@ -1,13 +1,12 @@
 import { Report } from '@/types/Reports/shared/Report';
-import { UserResponse } from '@/types/User/UserResponse';
-import AllUsers from "@/types/Reports/users/allUsers"
+import BasicUsers from "@/types/Reports/users/allUsers"
 import { User } from "@/types/User/columns"
 
 export const userRports = (): Report<User>[] => {
     return [
         {
-            title: 'All Users',
-            type: AllUsers
+            title: 'Basic Users',
+            type: BasicUsers
         }
     ]
 }
