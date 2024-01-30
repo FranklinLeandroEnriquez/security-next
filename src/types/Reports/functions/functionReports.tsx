57995f23dcbe5fd847a4b5510a0566321b5407dd
FranklinLeandroEnriquez <flenriquez@uce.edu.ec>
2024-01-30 18:25:08 -0500

    Add relationalFunctions to functionReports

--- src/types/Reports/functions/functionReports.tsx
+++ src/types/Reports/functions/functionReports.tsx
@@ -1,11 +1,16 @@
 import { Report } from '@/types/Reports/shared/Report';
 import BasicFunctions from './allFunctions';
+import { relationalFunctions } from './relationalFunctions';
 import { Function } from '@/types/Function/columns';
 export const functionReports = (): Report<Function>[] => {
     return [
         {
             title: 'Basic Functions',
             type: BasicFunctions
+        },
+        {
+            title: 'Relational Functions',
+            type: relationalFunctions
         }
     ]
 }
---
+++ src/types/Reports/functions/relationalFunctions.tsx
@@ -0,0 +1,66 @@
+import React, { useCallback, useEffect } from 'react';
+import { getFunction } from '@/services/Function/FunctionService';
+import { useAuthToken } from '@/hooks/useAuthToken';
+import { ErrorResponse } from '@/types/shared/ValidationError';
+import { toast } from 'sonner';
+import { ReporType } from '@/types/Reports/shared/Report';
+import { renderData } from '@/types/Reports/shared/FormatData';
+import { ReportHeader } from "@/types/Reports/shared/HeaderReport";
+import { FunctionResponse } from '@/types/Function/FunctionResponse';
+
+export function relationalFunctions<TData>({
+    table,
+}: ReporType<TData>) {
+
+    const [functions, setFunctions] = React.useState<FunctionResponse[]>([]);
+
+    const token = useAuthToken();
+
+    const getIdSelectedItems = useCallback((): number[] => {
+        const selectedIds = table?.getSelectedRowModel().flatRows.map((row) => {
+            const id = (row.original as any).id;
+            return id as number;
+        });
+        return selectedIds || [];
+    }, [table]);
+
+    const getFunctionHandler = useCallback(async (id: number): Promise<FunctionResponse> => {
+        const res = await getFunction(id, token);
+        if (res.status === 200) {
+            const data: FunctionResponse = await res.json();
+            return data;
+        } else {
+            const errorData: ErrorResponse = await res.json();
+            toast.error(errorData.message.toString());
+            throw new Error(errorData.message);
+        }
+    }, [token]);
+
+    const getFunctionsHandler = useCallback(async (ids: number[]): Promise<FunctionResponse[]> => {
+        const functions: FunctionResponse[] = [];
+
+        for (const id of ids) {
+            try {
+                const function_ = await getFunctionHandler(id);
+                functions.push(function_);
+            } catch (error) {
+                console.error(`Error obteniendo el usuario con ID ${id}: ${error}`);
+            }
+        }
+        return functions;
+    }, [getFunctionHandler]);
+
+    const data = renderData(functions, 0, "Function");
+
+    useEffect(() => {
+        const ids = getIdSelectedItems();
+        getFunctionsHandler(ids).then((functions) => {
+            setFunctions(functions);
+        });
+    }, []);
+
+    return (
+        <ReportHeader data={data} dataType='Functions' />
+    );
+}
+export default relationalFunctions;
