b1f096b4b691e660e92fd65d606af22d04b82994
SANTIAGO FIERRO <santimarcelofierrod@gmail.com>
2024-01-30 19:04:38 -0500

    Add RolesModulesFunctionsReport component to roleReports

--- src/types/Reports/roles/roleReports.tsx
+++ src/types/Reports/roles/roleReports.tsx
@@ -1,12 +1,17 @@
 import { Report } from '@/types/Reports/shared/Report';
 import basicRoles from "@/types/Reports/roles/allRoles"
 import { Role } from "@/types/Role/columns"
+import RolesModulesFunctionsReport from './RolesModulesFunctionsReport';
 
 export const roleReports = (): Report<Role>[] => {
     return [
         {
             title: 'Basic Roles',
             type: basicRoles
+        },
+        {
+            title: 'Complete Report',
+            type: RolesModulesFunctionsReport
         }
     ]
 }
